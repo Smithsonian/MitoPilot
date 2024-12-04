3261907a17cfd7c5ded47ca155a4c4f9bbc9f435
Daniel.MacGuigan <Daniel.MacGuigan@nmfs.local>
2024-12-04 09:32:43 -0500

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -106,14 +106,13 @@ add_external_resources <- function() {
     waiter::useWaiter(),
     rclipboard::rclipboardSetup(),
     shinyjs::useShinyjs()
-    tags$script(
-      '
-      Shiny.addCustomMessageHandler("scrollCallback",
-        function() {
-          var objDiv = document.getElementById("progress_div");
-          objDiv.scrollTop = objDiv.scrollHeight;
+    tags$script(HTML("
+      Shiny.addCustomMessageHandler('scrollToBottom', function(message) {
+        var modalContent = document.querySelector('.modal-content');
+        if (modalContent) {
+          modalContent.scrollTop = modalContent.scrollHeight;
         }
-      );'
-    )
+      });
+    "))
   )
 }
