bba9079f7343d671f1b6b79a9bfda264f525c488
Daniel.MacGuigan <Daniel.MacGuigan@nmfs.local>
2024-12-04 09:36:59 -0500

    Update app_run_pipline.R

--- R/app_run_pipline.R
+++ R/app_run_pipline.R
@@ -144,7 +144,7 @@ pipeline_server <- function(id) {
     # Render progress ----
     output$progress_out <- renderText({
       req(process_out())
-	  session$sendCustomMessage(type = "scrollCallback", 1)
+      session$sendCustomMessage("scrollToBottom", NULL)
     })
 
     # Stop ----
