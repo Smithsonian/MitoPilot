c87e8947a1addbea62e521ca4bd4e7b2b2a7f7db
Daniel.MacGuigan <Daniel.MacGuigan@nmfs.local>
2025-01-02 12:13:20 -0500

    Update app_assemble.R

--- R/app_assemble.R
+++ R/app_assemble.R
@@ -160,7 +160,7 @@ assemble_server <- function(id) {
               name = "Notes",
               html = TRUE,
               align = "left",
-              minminWidth = 150,
+              minWidth = 150,
               cell = rt_longtext()
             ),
             view = colDef(
