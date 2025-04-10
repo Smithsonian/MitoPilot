e190b6236622fb021a0b790588eadc08772a8ade
Dan MacGuigan <daniel.macguigan@gmail.com>
2025-04-10 12:54:23 -0400

    BUILD SITE

--- R/backwards_compatibility.R
+++ R/backwards_compatibility.R
@@ -1,7 +1,7 @@
 #' Update old project database for backwards compatibility
 #'
 #' Update old project database for backwards compatibility.
-#' Adds "reviewed", "ID_verified", "problematic" columns to the annotate table
+#' Adds "reviewed", "ID_verified", and "problematic" columns to the annotate table,
 #' "start_gene" column to the annotate_opts table,
 #' and "max_blast_hits" to the curate_opts table.
 #'
