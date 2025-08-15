b323bc84711850a48f5788ee8f56c8ef52ffc49a
Dan MacGuigan <daniel.macguigan@gmail.com>
2025-08-15 12:44:13 -0400

    update

--- R/app_run_pipline_userAsmb.R
+++ R/app_run_pipline_userAsmb.R
@@ -239,9 +239,8 @@ pipeline_server_userAsmb <- function(id) {
         if (any(grepl("Your job", submit_output, ignore.case = TRUE))) {
           shinyWidgets::sendSweetAlert(
             title = "Success!",
-            text = paste0(submit_output, ". You can monitor your job on Hydra with the `qstat` command. Job output will be logged to ",
-                          paste0(base_filename, ".log"),
-                          " in your project directory"),
+            text = paste0(submit_output, ". You can monitor your job on Hydra with the `qstat` command or see `",
+                          paste0(base_filename, ".log"), "` in your project directory"),
             type = "success"
           )
           removeModal()
