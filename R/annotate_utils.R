66e54e452e7b0fa7bbadf5204952998bfc32b05b
Dan MacGuigan <daniel.macguigan@gmail.com>
2025-12-10 16:16:28 -0500

    updating R env

--- R/annotate_utils.R
+++ R/annotate_utils.R
@@ -10,8 +10,8 @@
 compare_aa <- function(query, target, type = c("pctId", "similarity"), subMx = "BLOSUM80") {
   s1 <- Biostrings::AAString(query)
   s2 <- Biostrings::AAString(target)
-  data(list=subMx, package = "pwalign")
-  alignment <- pwalign::pairwiseAlignment(subject = s1, pattern = s2, substitutionMatrix = BLOSUM80)
+  #data(list=subMx, package = "pwalign")
+  alignment <- pwalign::pairwiseAlignment(subject = s1, pattern = s2, substitutionMatrix = subMx)
 
   # Return query-centric percent identity
   if (type[1] == "pctId") {
