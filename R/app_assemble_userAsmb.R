fe582ff2010e56719a39f0b0d9c4a1f683d57c2f
Dan MacGuigan <daniel.macguigan@gmail.com>
2026-09-11 03:06:05 -0400

    ui(assemble): topology cell wraps so a mixed sample shows both pills [T09]

--- R/app_assemble_userAsmb.R
+++ R/app_assemble_userAsmb.R
@@ -242,7 +242,8 @@ assemble_server_userAsmb <- function(id) {
               cell = rt_longtext()
             ),
             topology = colDef(
-              show = TRUE, class = .grp("topology"), headerClass = .grp("topology"),
+              show = TRUE, class = paste(.grp("topology"), "mp-note-cell"),
+              headerClass = .grp("topology"),
               minWidth = 120,
               align = "center",
               name = mp_col_name("topology"),
